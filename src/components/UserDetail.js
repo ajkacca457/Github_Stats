2d528fca0090c8fbc6baa033050f03a97348b751
Avijit Karmaker <ajkacca@gmail.com>
2022-10-23 23:25:36 +0300

    update userdetails

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const UserDetail = () => {
+  const {gituser}= useContext(AppContext);
+  const {avatar_url:img,bio,blog,company,name,twitter_username}=gituser;
+  console.log(img,bio,blog,company,name,twitter_username);
   return (
-    <div>User Details</div>
+    <div className='bg-indigo-300 flex gap-x-[10px] justify-center items-center p-8'>
+      <img src={img} alt={name} className="w-[20%]"/>
+      <div>
+        <h3>{name || "username"}</h3>
+        <p>{bio}</p>
+        <p>{company}</p>
+        <p>{blog}</p>
+        <a href={twitter_username}>Twitter</a>
+      </div>
+    </div>
   )
 }
 
