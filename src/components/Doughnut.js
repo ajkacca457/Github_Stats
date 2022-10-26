a5b3550e9fca36f00f600739d4608b176543654b
Avijit Karmaker <ajkacca@gmail.com>
2022-10-26 22:50:20 +0300

    add doughnut chart

--- src/components/Doughnut.js
+++ src/components/Doughnut.js
@@ -13,35 +13,27 @@ import FusionCharts from "fusioncharts";
 import Column2D from "fusioncharts/fusioncharts.charts";
 
 // Include the theme as fusion
-import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
 // Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
 
 
 // STEP 4 - Creating the DOM element to pass the react-fusioncharts component
-const Pie2D= ({data})=>{
+const Doughnut= ({data})=>{
 // STEP 3 - Creating the JSON object to store the chart configurations
 const chartConfigs = {
-  type: "pie2d", // The chart type
+  type: "doughnut2d", // The chart type
   width: "100%", // Width of the chart
   height: "400", // Height of the chart
   dataFormat: "json", // Data type
   dataSource: {
     // Chart Configuration
     chart: {
-      //Set the chart caption
-      caption: "Most language used",
-      //Set the chart subcaption
-      // subCaption: "In MMbbl = One Million barrels",
-      //Set the x-axis name
-      xAxisName: "Languages",
-      //Set the y-axis name
-      yAxisName: "Reserves (MMbbl)",
-      numberSuffix: "K",
+      caption: "Most Stars for language",
+      showPercentValues:0,
       decimals:0,
-      //Set the theme for your chart
-      theme: "fusion"
+      theme: "candy",
     },
     // Chart Data
     data
@@ -51,4 +43,4 @@ const chartConfigs = {
     return (<ReactFC {...chartConfigs} />);
 }
 
-export default Pie2D;
+export default Doughnut;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,30 +3,46 @@ import Fchart from './Fchart'
 import { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 import Pie2D from "./Pie2d";
+import Doughnut from './Doughnut';
 
 const Repos = () => {
-  const {gitrepos}= useContext(AppContext);
-  let languages=gitrepos.reduce((total,item)=>{
-    const {language}=item;
-    if(!language) return total;
-    if(!total[language]) {
-      total[language]={label:language,value:0}
-    } else {
-      total[language]={...total[language],value:total[language].value+1};
-    }
-    return total;
-  },{})
+const {gitrepos}= useContext(AppContext);
+let languages= gitrepos.reduce((total,item)=>{
+  const {language}=item;
+  if(!language) return total;
+  if(!total[language]) {
+    total[language]= {label:language,value:0}
+  }else {
+    total[language]={...total[language],value:total[language].value+1}
+  }  
+  return total;
+},{})
 
-languages= Object.values(languages).sort((a,b)=>{
+let stars=gitrepos.reduce((total,item)=>{
+  const {language,stargazers_count}=item;
+  if(!language) return total;
+  if(!total[language]) {
+    total[language]={label:language,value:stargazers_count}
+  } else {
+    total[language]={...total[language],value:total[language].value+stargazers_count}
+  }
+  return total;
+},{})
+
+languages=Object.values(languages).sort((a,b)=>{
   return b.value-a.value
 }).slice(0,5);
-console.log(languages);
+
+stars= Object.values(stars).sort((a,b)=>{
+  return b.value-a.value;
+}).slice(0,5)
 
   return (
     <div>
       Repos
       <Fchart/>
       <Pie2D data={languages}/>
+      <Doughnut data={stars}/>
     </div>
   )
 }
