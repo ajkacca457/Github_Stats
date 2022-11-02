5d644abbd4ea96e18614dc23534c6b6bc361bf09
Avijit karmaker <ajkacca@gmail.com>
2022-11-02 14:11:47 +0200

    update context, search, appcontext

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { useState } from 'react'
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Search = () => {
   const [user,setUser]=useState("");
+  const {requestlimit, error,getGithubUser}= useContext(AppContext);
 
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log(user);
+    if(user) {
+      getGithubUser(user);
+    }
   }
 
   return (
     <div>
+      <div className='limit'> <p>Remaining Requests: {requestlimit}/60</p></div>
+      {error.show && <div className='error bg-red-500 text-white text-center py-2 my-3'><p>{error.msg}</p></div>}
       <form onSubmit={handleSubmit}>
         <div className='form-group flex items-center justify-center gap-x-[5px]'>
         <input type="text" placeholder='search user..' className='border-2 border-indigo-300 w-full rounded-md py-1 px-2' value={user} onChange={(e)=>{setUser(e.target.value)}} />
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
 import { useState,createContext } from "react";
 import {user,repos,followers} from "./MockData/mockData";
 
 
 const AppContext= createContext();
+const rootUrl="https://api.github.com";
 
 
 const GithubProvider=({children})=>{
@@ -15,9 +17,45 @@ const GithubProvider=({children})=>{
     const [error, setError]= useState({show:false,msg:""});
 
 
+    const toggleError= (show=false,msg="")=> {
+        setError({show,msg});
+    }
+
+    const getRequestRate=async()=> {
+        const response= await fetch(`${rootUrl}/rate_limit`)
+        const data= await response.json();
+        const {rate: {remaining}}= data;
+        setRequestLimit(remaining);
+        if(remaining===0) {
+            toggleError(true,"It is not possible to make over 60 request in an hour. Please try again")
+        }
+    }
+
+    const getGithubUser=async(user)=> {
+        setIsloading(true);
+        toggleError();
+        try {
+            const response= await fetch(`${rootUrl}/users/${user}`);
+            console.log(response);
+            if(response.ok) {
+                const data= await response.json();
+                setGitUser(data);
+            }else {
+                toggleError(true,"no user exists by this name");
+             }
+            setIsloading(false);                          
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    useEffect(()=> {
+        getRequestRate();
+    },[])  
 
     return(
-        <AppContext.Provider value={{gituser,gitrepos,gitfollowers}}>
+        <AppContext.Provider value={{gituser,gitrepos,gitfollowers,requestlimit, error,getGithubUser}}>
             {children}
         </AppContext.Provider>
     )
